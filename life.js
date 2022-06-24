9012c9d60bc50ba87340e9a38d32b63b1c61640e
Manna86 <samuelademola943@gmail.com>
2022-06-24 10:34:17 +0100

    first commit

---
+++ life.js
@@ -0,0 +1,9 @@
+let MYname = "Oseni Samuel";
+let height = "5 feet";
+let country = "Nigeria";
+
+console.log(MYname);
+console.log(height);
+console.log(country);
+
+alert('I am Oseni Samuel \n\nI am five feet tall \n\nI am a from Nigeria');
